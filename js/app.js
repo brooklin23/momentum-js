6f405ca5db4000136993d6194028260b019cc3a9
zun0323 <zun0323@gmail.com>
2022-07-18 17:48:14 -0300

    #4.2 Events

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
+function onLoginBtnClick(event) {
+  event.preventDefault();
   const userName = loginInput.value;
   console.log(userName);
 }
