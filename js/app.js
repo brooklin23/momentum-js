f35dbb5618724ed2c10b62d05dc98316bfc9574e
zun0323 <zun0323@gmail.com>
2022-07-18 17:00:42 -0300

    #4.1 Form Submission

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-  console.log("hello ", loginInput.value);
+  const userName = loginInput.value;
+  console.log(userName);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
