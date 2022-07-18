c11827f00f68c6252bf57bac092c4572034159da
zun0323 <zun0323@gmail.com>
2022-07-18 09:40:00 -0300

    #3.6 Css in JavaScript

--- js/app.js
+++ js/app.js
@@ -1,46 +1,16 @@
 const title = document.querySelector(".title__container");
 const body = document.querySelector("body");
 
-title.innerText = "hello";
-
 function changeColor() {
-  title.style.color = "blue";
+  let currentColor = title.style.color;
+  if (currentColor === "blue") {
+    console.log("it's blue");
+    currentColor = "tomato";
+  } else {
+    currentColor = "blue";
+    console.log("it's tomato");
+  }
+  title.style.color = currentColor;
 }
 
-function changeBackgrounColor() {
-  body.style.backgroundColor = "tomato";
-}
-
-function changeTextIn() {
-  title.innerText = "Mause is Here~";
-}
-
-function changeTextOut() {
-  title.innerText = "Mouse has Gone!!";
-}
-
-function changeWholeColor() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleCopy() {
-  alert("Copier!");
-}
-
-function handleWindowOffline() {
-  alert("Wifi disconnected!!!");
-}
-
-function handleWindowOnline() {
-  alert("All Goooood~");
-}
-
-title.addEventListener("mouseenter", changeTextIn);
-title.addEventListener("mouseleave", changeTextOut);
-
-window.addEventListener("resize", changeWholeColor);
-
-window.addEventListener("copy", handleCopy);
-
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+title.addEventListener("click", changeColor);
