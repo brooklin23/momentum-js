965513d384e98fe6631823a10b96996fc1358e95
zun0323 <zun0323@gmail.com>
2022-07-19 14:52:20 -0300

    #4.6 Loading Username

--- js/app.js
+++ js/app.js
@@ -2,14 +2,26 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
 function onLoginSubmit(event) {
-  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const userName = loginInput.value;
-  loginForm.classList.add("hidden");
-  greeting.classList.remove("hidden");
-  localStorage.setItem("username", userName);
-  greeting.innerText = `Hello ${userName}`;
-  console.log(userName);
+  localStorage.setItem(USERNAME_KEY, userName);
+  paintGreeting(savedUsername);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreeting(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreeting(savedUsername);
+}
