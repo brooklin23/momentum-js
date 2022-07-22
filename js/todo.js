7510f750cacf1f029ce2ffe038e5a528dd83ec7e
zun0323 <zun0323@gmail.com>
2022-07-22 12:48:10 -0300

    #7.1 Adding Todos_start

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function paingToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  console.log(li);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  console.log(newTodo);
+  paingToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
